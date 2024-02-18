d9b744605d2751119ae2f342b30ddfdf058b1568
xEmmanuelCobax <emmanuelcobacuevas@gmail.com>
2024-02-18 14:36:32 -0600

    v1

---
+++ src/App.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__) 
+
+@app.route('/')
+def Hola(): 
+    data={
+        'titulo':'Página plantilla',
+        'mensaje':'Bienvenido al sitio Web ',
+        'nombre' : 'Emmanuel Jesus Coba Cuevas'
+        }
+    return render_template('pagina1.html',data=data) 
+app.run(debug=True) 
