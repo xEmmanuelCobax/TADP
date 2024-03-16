37de13865b5b8bea907172488889029665ace50f
xEmmanuelCobax <emmanuelcobacuevas@gmail.com>
2024-03-16 16:47:03 -0600

    v1.6

--- src/App.py
+++ src/App.py
@@ -1,16 +1,15 @@
 # author: Emmanuel Jesus Coba Cuevas
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-import os
-import json
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory,session
+import os, config
+from flask_mysqldb import MySQL
 
-try:
-    with open('usuarios.json', 'r') as file:
-        usuarios = json.load(file)
-except FileNotFoundError:
-    usuarios = {}
-
-user = ""
 app = Flask(__name__) 
+app.config["MYSQL_USER"] = config.MYSQL_USER
+app.config["MYSQL_DB"] = config.MYSQL_DB
+app.config["MYSQL_PASSWORD"] = config.MYSQL_PASSWORD
+app.config['SECRET_KEY'] = config.HEX_SEC_KEY
+
+mysql = MySQL(app)
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -20,39 +19,60 @@ def send_static(path):
 def index(): 
     return render_template('index.html') 
 
-#Registarse
+
 @app.route('/sign', methods=['GET', 'POST'])
 def sign():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
 
-        if username in usuarios and usuarios[username] == password:
-            return redirect (url_for('saludo', user=username))
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        user = cur.fetchone()
+        cur.close()
+
+        if user:
+            session['email'] = email
+            return redirect(url_for('saludo', user=email))
 
         else:
-            error_message = "Usuario o contraseña incorrecta"
-            return render_template ('sign.html', error_message=error_message)
+            error_message = "Email o contraseña incorrecta"
+            return render_template('sign.html', error_message=error_message)
+        
     return render_template('sign.html') 
-#Acceder
+
 @app.route('/signup',  methods=['GET', 'POST'])
 def signup(): 
     if request.method == 'POST':
-        username = request.form.get('username')
+        name = request.form.get('username')
+        email = request.form.get('email')
         password = request.form.get('password')
-        print(username)
-        print(password)
-        if username in usuarios:
-            error_message = "El usuario ya existe. Por favor, elige otro nombre de usuario."
-            return render_template ('signup.html', error_message=error_message)
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+        existing_email = cur.fetchone()
+        
+        cur.execute("SELECT * FROM users WHERE name = %s", (name,))
+        existing_user = cur.fetchone()
+
+        if existing_email:
+            error_message = "El correo electrónico ya está registrado. Por favor, utiliza otro correo electrónico."
+            return render_template('signup.html', error_message=error_message)
+        
+        elif existing_user:
+            error_message = "El usuario ya existe. Por favor, elige otro correo electrónico."
+            return render_template('signup.html', error_message=error_message)
         else:
-            usuarios[username] = password
-            try:
-                with open('usuarios.json', 'w') as file:
-                    json.dump(usuarios, file, indent=4)
-            except Exception as e:
-                print(f"Error al escribir en el archivo JSON: {e}")
+            cur.execute("INSERT INTO users (name,email, password) VALUES (%s, %s, %s)", (name, email, password))
+            mysql.connection.commit()
+            session['email'] = email
+            return redirect(url_for('registro_exitoso'))
+
     return render_template('signup.html')  
+
+@app.route('/registro_exitoso')
+def registro_exitoso():
+    return render_template('registro_exitoso.html')
 
 @app.route('/page1/<user>')
 def saludo(user):
