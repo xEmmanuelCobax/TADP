335c43e0dfeeae4461304f9099be2354e2d54f29
xEmmanuelCobax <emmanuelcobacuevas@gmail.com>
2024-03-16 19:27:52 -0600

    v1.6.2

--- src/App.py
+++ src/App.py
@@ -1,5 +1,4 @@
-# author: Emmanuel Jesus Coba Cuevas
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory,session
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
 import os, config
 from flask_mysqldb import MySQL
 
@@ -7,21 +6,18 @@ app = Flask(__name__)
 app.config["MYSQL_USER"] = config.MYSQL_USER
 app.config["MYSQL_DB"] = config.MYSQL_DB
 app.config["MYSQL_PASSWORD"] = config.MYSQL_PASSWORD
-app.config['SECRET_KEY'] = config.HEX_SEC_KEY
+app.config['SECRET_KEY'] = config.HEX_SEC_KEY  # Configurar la clave secreta
 
 mysql = MySQL(app)
 
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
-
 @app.route('/')
 def index(): 
+    print (session)
     return render_template('index.html') 
-
 
 @app.route('/sign', methods=['GET', 'POST'])
 def sign():
+    print (session)
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -33,16 +29,16 @@ def sign():
 
         if user:
             session['email'] = email
-            return redirect(url_for('saludo', user=email))
+            return redirect(url_for('perfil', user=email))
 
         else:
             error_message = "Email o contraseña incorrecta"
             return render_template('sign.html', error_message=error_message)
-        
     return render_template('sign.html') 
 
 @app.route('/signup',  methods=['GET', 'POST'])
 def signup(): 
+    print (session)
     if request.method == 'POST':
         name = request.form.get('username')
         email = request.form.get('email')
@@ -72,25 +68,33 @@ def signup():
 
 @app.route('/registro_exitoso')
 def registro_exitoso():
+    print (session)
     return render_template('registro_exitoso.html')
 
 @app.route('/page1/<user>')
-def saludo(user):
-    return '<h2>hola '+ user +'</h2>'
+def perfil(user):
+    if 'email' in session:
+        email = session['email']
+        return render_template('perfil.HTML', email=email)
+    else:
+        return redirect(url_for('sign'))
 
 @app.route('/buscar', methods=['GET'])
-
 def buscar():
     query = request.args.get('query')  # Obtener la consulta de búsqueda de la URL
     if query == 'index':
-        return redirect(url_for('index'))  # Redirigir a la página 'Conocerme'
+        return redirect(url_for('index'))  # Redirigir a la página 'Index'
     elif query == 'signup':
-        return redirect(url_for('signup'))  # Redirigir a la página 'Formacion'
+        return redirect(url_for('signup'))  # Redirigir a la página 'signup'
     elif query == 'sign':
-        return redirect(url_for('sign'))  # Redirigir a la página 'Habilidades'
+        return redirect(url_for('signup'))  # Redirigir a la página 'Habilsignupidades'
     else:
-        return render_template('404.html', query=query)  # Página no encontrada
-    
+        return render_template('404.html', query=query)  # Página no encontrada 404
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
