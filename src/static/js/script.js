1cf6779006c9845e9600d0d19792a8737491f9bf
xEmmanuelCobax <emmanuelcobacuevas@gmail.com>
2024-03-18 08:55:31 -0600

    v1.8

---
+++ src/static/js/script.js
@@ -0,0 +1,13 @@
+const light = () => {
+    document.querySelector("body").setAttribute("data-bs-theme","light")
+    document.getElementById("theme").setAttribute("xlink:href", "#sun-fill");
+    document.getElementById("lightButton").classList.add("active");
+    document.getElementById("darkButton").classList.remove("active");
+}
+
+const dark = () => {
+    document.querySelector("body").setAttribute("data-bs-theme","dark")
+    document.getElementById("theme").setAttribute("xlink:href", "#moon-fill");
+    document.getElementById("lightButton").classList.remove("active");
+    document.getElementById("darkButton").classList.add("active");
+}
